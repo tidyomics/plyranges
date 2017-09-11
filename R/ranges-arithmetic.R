8095016acbebba5ca1b7247249486c7b0df37795
Stuart Lee <lee.s@wehi.edu.au>
2017-09-11 12:49:28 +1000

    A non-standard evaluation approach for anchoring Ranges. Allows for piping.

--- R/ranges-arithmetic.R
+++ R/ranges-arithmetic.R
@@ -1,37 +1,53 @@
 # arithemtic-ranges.R
-#' @importFrom rlang quo
-stretch <- function(x, extend = 0L) {
+anchor_start <- function(x) {
+  cl <- match.call()
+  cl$x <- enquo(x)
+  list(cl = cl, x = x)
 
-  expr <- enquo(x)
-  print(expr)
-  print(quo_name(expr))
-  if (grepl("start", quo_name(expr))) {
-    quos(width = width + UQ(extend))
-  } else if (grepl("end", quo_name(expr))) {
-    quos(start = end - width + UQ(extend) + 1L)
-  } else if (grepl("center", quo_name(expr))) {
-    midpt <- eval_tidy(expr)
-    if (sign(extend) == -1) {
-      quos(start = start + 2*midpt, end = start)
-    } else {
-      quos(start = end, end = end + 2*midpt)
-    }
-  } else {
-    !!!quos(shift(x, !!extend))
-  }
-
-}
-
-anchor_start <- function() {
-  stop("anchor_start should only be called inside another function",
-       .call = FALSE)
 }
 
 anchor_end <- function(x) {
-  end(x)
+  cl <- match.call()
+  cl$x <- enquo(x)
+  list(cl = cl, x = x)
 }
 
 anchor_center <- function(x) {
-  (end(x) - start(x)) / 2
+  cl <- match.call()
+  cl$x <- enquo(x)
+  list(cl = cl, x = x)
 }
+
+#' Extend the width of a genomic interval
+#'
+#' @param x a Ranges object, to fix by either the start, end or center
+#' of an interval use \code{anchor_start(x)}, \code{anchor_end(x)},
+#' \code{anchor_center(x)}.
+#' @param width the amount to stretch a Ranges object by. Either an
+#' integer vector of length 1 or the same length as x.
+#' @importFrom rlang quo enquo eval_tidy
+#' @importFrom IRanges resize
+#' @seealso \code{\link[IRanges]{resize}}
+stretch <- function(x, width = 0L) {
+
+  expr <- enquo(x)
+  qexpr <- eval_tidy(UQ(expr))
+  if (!inherits(qexpr, "Ranges")) {
+    fun_name <- gsub("\\(.*", "",qexpr$cl[[1]])
+    x <- qexpr$x
+  } else {
+    x <- qexpr
+    width(x) <- width
+    return(x)
+  }
+
+  resize_expr <- switch(fun_name,
+                        anchor_start = quo(resize(!!x, width = !!width, fix = "start")),
+                        anchor_end = quo(resize(!!x, width = !!width, fix = "end")),
+                        anchor_center = quo(resize(!!x, width = !!width, fix = "center")),
+                        )
+  eval_tidy(resize_expr)
+
+}
+
 
