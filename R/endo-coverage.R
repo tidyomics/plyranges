5e73d468e8aef17b95e85473993c95730daff31b
Stuart Lee <lee.s@wehi.edu.au>
2017-09-20 13:47:16 +1000

    Rename of coverage method to be consistent, updated bedtools examples.

--- R/endo-coverage.R
+++ R/endo-coverage.R
@@ -6,35 +6,27 @@
 #' This must be either a  positive integer, NA or NULL (default = NULL)
 #' @param weight weight how much weight should be assigned to each range? Either
 #' an integer or numeric vector or a column in x. (default = 1L)
-#' @param method method see \link[IRanges]{coverage}.
 #'
 #' @return An expanded Ranges object with a score column corresponding to
 #' the coverage value over that interval. Note that compute_coverage
 #' drops metadata associated with the orginal ranges.
 #' @seealso \link[IRanges]{coverage}
-#' @importFrom IRanges coverage
+#' @importFrom IRanges coverage ranges runValue
 #' @export
-#' @rdname compute_coverage
-#' @name compute_coverage.rd
-setGeneric("compute_coverage",
-           function(x, shift = 0L, width = NULL, weight = 1L,
-                    method = c("auto", "sort", "hash")) {
-             standardGeneric("compute_coverage")
-             })
+#' @rdname set_coverage
+set_coverage <- function(x, shift, width, weight) {
+  UseMethod("set_coverage")
+}
 
-#' @describeIn compute_coverage compute the coverage of a GRanges object
-setMethod("compute_coverage", "GRanges",
-          function(x, shift = 0L, width = NULL, weight = 1L,
-                   method = c("auto", "sort", "hash")) {
-            GRanges(coverage(x, shift, width, weight, method))
-          })
+#' @rdname set_coverage
+#' @export
+set_coverage.GenomicRanges <- function(x, shift = 0L, width = NULL, weight = 1L) {
+  GRanges(coverage(x, shift, width, weight, method = "auto"))
+}
 
-#' @describeIn compute_coverage compute the coverage of an IRanges object
-setMethod("compute_coverage", "IRanges",
-          function(x, shift = 0L, width = NULL, weight = 1L,
-                   method = c("auto", "sort", "hash")) {
-            cvg <- coverage(x, shift, width, weight, method)
-            rng <- ranges(cvg)
-            mcols(rng)[["score"]] <- runValue(cvg)
-            rng
-          })
+set_coverage.Ranges <- function(x, shift = 0L, width = NULL, weight = 1L) {
+  cvg <- coverage(x, shift, width, weight, method)
+  rng <- ranges(cvg)
+  mcols(rng)[["score"]] <- runValue(cvg)
+  rng
+}
