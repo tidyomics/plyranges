96d7935ccdf75164460e3bc4befe13a85ee3b3ef
Stuart Lee <lee.s@wehi.edu.au>
2017-11-06 13:50:05 +1100

    docs for select, unit tests for select.

--- R/dplyr-select.R
+++ R/dplyr-select.R
@@ -1,61 +1,89 @@
-select_rng <- function(.data, ...) {
-  dots <- UQS(...)
-  col_names <- unlist(lapply(dots, quo_name))
-  col_syms <- syms(col_names)
-  names(col_syms) <- col_names
+select_rng <- function(.data, .drop_ranges, ...) {
 
+  if (.drop_ranges) {
+    var_names <- ranges_vars(.data)
+    selected_vars <- tidyselect::vars_select(var_names, UQS(...))
+  } else {
+    var_names <- names(mcols(.data))
+    if (length(var_names) == 0L) {
+      stop("No metadta columns to select", call. = FALSE)
+    }
+
+    if (is(.data, "Ranges")) {
+      exclude <- c("start", "end", "width")
+    } else {
+      exclude <- c("start", "end", "width", "strand", "seqnames")
+    }
+
+    selected_vars <- tidyselect::vars_select(var_names, UQS(...),
+                                             .exclude = exclude)
+  }
+
+  selected_vars <- syms(selected_vars)
   rng_os <- overscope_ranges(.data)
   on.exit(overscope_clean(rng_os))
+  rng_list <- lapply(selected_vars, overscope_eval_next, overscope = rng_os)
 
-  rng_df <- lapply(col_syms, overscope_eval_next, overscope = rng_os)
-  rng_df <- as_tibble(as.data.frame(rng_df))
-
-  rng <- try(Ranges(rng_df), silent = TRUE)
-
-  if (is(rng, "try-error")) {
-    return(rng_df)
+  if (.drop_ranges) {
+    return(as_tibble(as.data.frame(rng_list)))
   } else {
-    return(rng)
+    if (length(rng_list) > 0) {
+      mcols(.data) <- do.call("DataFrame", rng_list)
+    } else {
+      mcols(.data) <- NULL
+    }
+
+    return(.data)
   }
 }
 
 
-#' Select parts of the GRanges by name
+#' Select metadata columns of the Ranges object by name or position
 #'
 #' @param .data a \code{Ranges} object
-#' @param ... One or more column names including the core components
-#' of the Ranges object - if possible select will preserve the Ranges object,
-#' however in cases where this is not possible select will return a tibble.
-#' @details Note that if a core component of a Ranges is dropped or selected
+#' @param ... One or more metadata column names.
+#' @param .drop_ranges If TRUE select will always return a tibble. In this
+#' case, you may select columns that form the core part of the Ranges object.
+#' @details Note that by default select only acts on the metadata columns (and
+#' will therefore return a Ranges object) if a core component of a Ranges is dropped or selected
 #' without the other required components (this includes the seqnames, strand, start, end,
-#' width names), then select will return a tibble.
-#' @note select doesn't currently support slicing syntax or integer based selection.
+#' width names), then select will throw an error unless .drop_ranges is set to TRUE.
 #' @return a Ranges object or a tibble
 #' @seealso \link[dplyr]{select}
 #' @importFrom dplyr select
+#' @importFrom tidyselect vars_select
 #' @rdname ranges-select
 #' @method select GenomicRanges
+#' @examples
+#' df <- data.frame(start = 1:10, width = 5,  seqnames = "seq1",
+#' strand = sample(c("+", "-", "*"), 10, replace = TRUE), gc = runif(10), counts = rpois(10, 2))
+#' rng <- Ranges(df)
+#' select(rng, -gc)
+#' select(rng, gc)
+#' select(rng, counts, gc)
+#' select(rng, 2:1)
+#' select(rng, seqnames, strand, .drop_ranges = TRUE)
 #' @export
-select.GenomicRanges <- function(.data, ...) {
+select.GenomicRanges <- function(.data, ..., .drop_ranges = FALSE) {
 
   dots <- quos(...)
-
   # no selection? - return .data
   if (length(dots) == 0) {
     return(.data)
   }
 
   # if the quo is named return an error
-  stopifnot(length(names(dots)) != 0)
+  if (length(names(dots)) == 0) {
+    stop("select does not support renaming variables", call. = FALSE)
+  }
 
-  select_rng(.data, dots)
-
+  select_rng(.data, .drop_ranges, dots)
 }
 
 #' @rdname ranges-select
 #' @method select Ranges
 #' @export
-select.Ranges <- function(.data, ...) {
+select.Ranges <- function(.data, ..., .drop_ranges = FALSE) {
   dots <- quos(...)
 
   # no selection? - return .data
@@ -64,7 +92,9 @@ select.Ranges <- function(.data, ...) {
   }
 
   # if the quo is named return an error
-  stopifnot(length(names(dots)) != 0)
+  if (length(names(dots)) == 0) {
+    stop("select does not support renaming variables", call. = FALSE)
+  }
 
-  select_rng(.data, dots)
+  select_rng(.data, .drop_ranges,  dots)
 }
---
+++ tests/testthat/test-select.R
@@ -0,0 +1,52 @@
+context("Column wise selection")
+
+gr0 <- GRanges()
+ir0 <- IRanges()
+
+mdata <- DataFrame(score = as.integer(c(0,3,4,5,10,14,0, 0, 4,8)),
+                   gc = runif(10),
+                   counts = rpois(10, 2))
+ir1 <- IRanges(start = 1:10, width = 5)
+mcols(ir1) <- mdata
+gr1 <- GRanges(seqnames = "seq1",
+               strand = c(rep("+", 4), rep("-", 3), rep("*", 3)),
+               ranges = ir1)
+
+
+test_that("no selection returns the same object", {
+  expect_identical(select(ir0), ir0)
+  expect_identical(select(gr0), gr0)
+  expect_identical(select(ir1), ir1)
+  expect_identical(select(gr1), gr1)
+})
+
+test_that("dropping core parts throws an error...", {
+  expect_error(select(ir0, start))
+  expect_error(select(gr0, start))
+})
+
+test_that("...unless allowing to drop ranges", {
+  expect_s3_class(select(ir0, start, .drop_ranges = TRUE), "tbl_df")
+  expect_s3_class(select(gr0, start, .drop_ranges = TRUE), "tbl_df")
+})
+
+test_that("reordering/dropping works as expected", {
+  gr2 <- select(gr1, counts, gc, score)
+  expect_equal(names(mcols(gr2)), c("counts", "gc", "score"))
+  gr3 <- select(gr1, -1)
+  gr4 <- select(gr1, -score)
+  expect_identical(gr3, gr4)
+  ir2 <- select(ir1, -counts)
+  expect_equal(names(mcols(ir2)), c("score", "gc"))
+  ir3 <- select(ir1, 1:2)
+  expect_identical(ir2, ir3)
+})
+
+test_that("dropping everything sets mcols slot to empty", {
+  gr5 <- select(gr1, -score:-counts)
+  mcols(gr1) <- NULL
+  expect_identical(gr5, gr1)
+  ir4 <- select(ir1, -score:-counts)
+  expect_identical(ir4, IRanges(ir1))
+})
+
