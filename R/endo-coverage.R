91a96b519c2dc52b776f64241bade7c95cabe67d
Stuart Lee <lee.s@wehi.edu.au>
2017-12-06 15:14:46 +1100

    rename of set_coverage -> compute_coverage,  first part of #14

--- R/endo-coverage.R
+++ R/endo-coverage.R
@@ -1,9 +1,9 @@
-#' Ranges friendly coverage method
+#' Compute coverage over a Ranges object
 #'
 #' @param x a \code{Ranges} object
 #' @param shift shift how much should each range in x be shifted by? (default = 0L)
 #' @param width width how long should the returned coverage score be?
-#' This must be either a  positive integer, NA or NULL (default = NULL)
+#' This must be either a  positive integer or NULL (default = NULL)
 #' @param weight weight how much weight should be assigned to each range? Either
 #' an integer or numeric vector or a column in x. (default = 1L)
 #'
@@ -11,31 +11,33 @@
 #' the coverage value over that interval. Note that compute_coverage
 #' drops metadata associated with the orginal ranges.
 #' @seealso \link[IRanges]{coverage}
+#' @examples
+#' rng <- as_iranges(data.frame(start = 1:10, width = 5))
+#' compute_coverage(rng)
+#' compute_coverage(rng, shift = 14L)
+#' compute_coverage(rng, width = 10L)
 #' @importFrom IRanges coverage ranges
 #' @importFrom S4Vectors runValue
 #' @export
-#' @rdname set_coverage
-set_coverage <- function(x, shift, width, weight) {
-  UseMethod("set_coverage")
+compute_coverage <- function(x, shift, width, weight) {
+  UseMethod("compute_coverage")
 }
 
-#' @rdname set_coverage
 #' @export
-set_coverage.GenomicRanges <- function(x, shift = 0L, width = NULL, weight = 1L) {
+compute_coverage.GenomicRanges <- function(x, shift = 0L, width = NULL, weight = 1L) {
   GRanges(coverage(x, shift, width, weight, method = "auto"))
 }
 
-set_coverage.GRangesGrouped <- function(x, shift = 0L, width = NULL, weight = 1L) {
-  gr_groups <- split_groups(x)
-  # this will drop grouping
-  unlist(endoapply(gr_groups, function(x) {
-    set_coverage.GenomicRanges(x, shift, width, weight)
-    }))
-}
-
-set_coverage.Ranges <- function(x, shift = 0L, width = NULL, weight = 1L) {
+#' @export
+compute_coverage.Ranges <- function(x, shift = 0L, width = NULL, weight = 1L) {
   cvg <- coverage(x, shift, width, weight, method = "auto")
   rng <- ranges(cvg)
   mcols(rng)[["score"]] <- runValue(cvg)
   rng
 }
+
+# compute_coverage.GRangesGrouped <- function(x, shift = 0L, width = NULL, weight = 1L) {
+#   gr_groups <- split_groups(x)
+#   GRanges(coverage(gr_groups, shift, width, weight, method = "auto"))
+# }
+
--- tests/testthat/test-coverage.R
+++ tests/testthat/test-coverage.R
@@ -1,23 +1,72 @@
 # test-coverage.R
 # adapted from HelloRanges tests
 context("coverage")
-stopifnot(requireNamespace("HelloRanges"))
-oldwd <- getwd()
-setwd(system.file("unitTests", "data", "genomecov", package="HelloRanges"))
 
-genome <- read.delim("test.genome", header = FALSE)
+test_that("produces same result as IRanges",
+          {
+            ir <- IRanges(c(1, 8, 14, 15, 19, 34, 40),
+                          width = c(12, 6, 6, 15, 6, 2, 7))
+            cvg <- compute_coverage(ir)
+            expect_identical(mcols(cvg)$score,
+                             c(1L, 2L, 1L, 2L, 3L, 2L, 1L, 0L, 1L, 0L, 1L))
+            expect_identical(width(cvg),
+                             c(7L, 5L, 2L, 4L, 1L, 5L, 5L, 4L, 2L, 4L, 7L))
 
-genome <- genome_info(genome = "test",
-                      seqnames = as.character(genome$V1),
-                      seqlengths = genome$V2)
+            ir <- IRanges(start=c(-2L, 6L, 9L, -4L, 1L, 0L, -6L, 10L),
+                          width=c( 5L, 0L, 6L,  1L, 4L, 3L,  2L,  3L))
+            cvg <- compute_coverage(ir)
+            expect_identical(mcols(cvg)$score,
+                             c(3L, 1L, 0L, 1L, 2L, 1L))
+            expect_identical(width(cvg), c(2L, 2L, 4L, 1L, 3L, 2L))
 
+            cvg <- ir %>% shift_right(7L) %>% compute_coverage()
+            expect_identical(mcols(cvg)$score,
+                             c(1L, 0L, 1L, 2L, 3L, 1L, 0L, 1L, 2L, 1L))
+            expect_identical(width(cvg),
+                             c(3L, 1L, 2L, 1L, 2L, 2L, 4L, 1L, 3L, 2L))
+
+          })
+
+test_that("produces same results as GRanges", {
+  gr <- GRanges(seqnames = Rle(factor(c("chr1", "chr2", "chr1", "chr3")),
+                               c(1, 3, 2, 4)),
+                ranges = IRanges(1:10, width = 10:1))
+  target <- RleList(chr1=Rle(1:3, c(4, 1, 5)),
+                    chr2=Rle(0:3, c(1, 1, 1, 7)),
+                    chr3=Rle(0:4, c(6, 1, 1, 1, 1)),
+                    compress=FALSE)
+  cvg <- compute_coverage(gr)
+
+  expect_identical(cvg$score, unlist(runValue(target), use.names = FALSE))
+  expect_identical(width(cvg), unlist(runLength(target), use.names = FALSE))
+
+  target <- RleList(chr1=Rle(1:3, c(4, 1, 5)),
+                    chr2=Rle(c(0:3, 0L), c(1, 1, 1, 7, 10)),
+                    chr3=Rle(c(0:4, 0L), c(6, 1, 1, 1, 1, 20)),
+                    compress=FALSE)
+
+  cvg <- compute_coverage(gr, width = c(chr1 = 10, chr2 = 20, chr3 = 30))
+
+  expect_identical(cvg$score, unlist(runValue(target), use.names = FALSE))
+  expect_identical(width(cvg), unlist(runLength(target), use.names = FALSE))
+
+})
 test_that("Compatible with GenomicRanges", {
+  stopifnot(requireNamespace("HelloRanges"))
+  oldwd <- getwd()
+  setwd(system.file("unitTests", "data", "genomecov", package="HelloRanges"))
+
+  genome <- read.delim("test.genome", header = FALSE)
+
+  genome <- genome_info(genome = "test",
+                        seqnames = as.character(genome$V1),
+                        seqlengths = genome$V2)
   y <- read_bed("y.bed", genome_info = genome)
   cov_gr <- as(coverage(granges(y)), "GRanges")
   exp <- cov_gr[score(cov_gr) > 0]
-  cov_plyr <- y %>% set_coverage() %>% filter(score > 0)
+  cov_plyr <- y %>% compute_coverage() %>% filter(score > 0)
   expect_identical(exp, cov_plyr)
+  setwd(oldwd)
 
 })
 
-setwd(oldwd)
