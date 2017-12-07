217607f483bd2e0d3b4a8e1e2d4468bae647d686
Stuart Lee <lee.s@wehi.edu.au>
2017-12-07 15:12:08 +1100

    flank bug fix, addressing #21

--- R/ranges-arithmetic-flank.R
+++ R/ranges-arithmetic-flank.R
@@ -4,29 +4,42 @@
 #'
 #' @param x a Ranges object .
 #' @param width the width of the flanking range relative to the genomic interval
-#' Ranges object by. Either an integer vector of length 1 or an integer vector the same
-#' length as x. The width can be negative in which case the flanking region
-#' is reversed.
+#' Ranges object by. Either an integer vector of length 1 or an integer vector
+#' the same length as x. The width can be negative in which case the
+#' flanking region is reversed.
+#'
+#' @details By default \code{flank_left} and \code{flank_right} will
+#' ignore strandedness of any ranges, while flank upstream and
 #' @seealso \code{\link[IRanges]{flank}}
 #' @importFrom IRanges flank
+#' @examples
+#' gr <- as_granges(data.frame(start = 10:15,
+#'                             width = 5,
+#'                             seqnames = "seq1",
+#'                             strand = c("+", "+", "-", "-", "+", "*")))
+#' flank_left(gr, width = 5L)
+#' flank_right(gr, width = 5L)
+#' flank_upstream(gr, width = 5L)
+#' flank_downstream(gr, width = 5L)
 #' @rdname flank-ranges
 #' @export
 flank_left <- function(x, width = 0L) {
   stopifnot(is.numeric(width))
-  flank(x, width, start = TRUE)
+  flank(x, width, start = TRUE, ignore.strand = TRUE)
 }
 
 #' @rdname flank-ranges
 #' @export
 flank_right <- function(x, width = 0L) {
   stopifnot(is.numeric(width))
-  flank(x, width, start = FALSE)
+  flank(x, width, start = FALSE, ignore.strand = TRUE)
 }
 
 #' @rdname flank-ranges
 #' @export
 flank_upstream <- function(x, width = 0L) {
   stopifnot(is.numeric(width))
+  stopifnot(is(x, "GenomicRanges"))
   flank(x, width, start = TRUE, ignore.strand = FALSE)
 }
 
@@ -34,5 +47,6 @@ flank_upstream <- function(x, width = 0L) {
 #' @export
 flank_downstream <- function(x, width = 0L) {
   stopifnot(is.numeric(width))
+  stopifnot(is(x, "GenomicRanges"))
   flank(x, width, start = FALSE, ignore.strand = FALSE)
 }
