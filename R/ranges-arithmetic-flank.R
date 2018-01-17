785f7ad716593bd408d89b332fd5b59fa7410d9d
Stuart Lee <lee.s@wehi.edu.au>
2018-01-17 14:45:11 +1100

    update to flanks documenation

--- R/ranges-arithmetic-flank.R
+++ R/ranges-arithmetic-flank.R
@@ -1,16 +1,32 @@
 # ranges-arithmetic-flank.R
 
-#' Shift all coordinates in a genomic interval left or right, upstream or downstream
-#'
-#' @param x a Ranges object .
-#' @param width the width of the flanking range relative to the genomic interval
-#' Ranges object by. Either an integer vector of length 1 or an integer vector
+#' Generate flanking regions
+#' @description Find flanking regions  to the left or right or
+#' upstream or downstream of a Ranges object.
+
+#' @param x a Ranges object.
+#' @param width the width of the flanking region relative to the ranges in
+#' \code{x}. Either an integer vector of length 1 or an integer vector
 #' the same length as x. The width can be negative in which case the
 #' flanking region is reversed.
 #'
-#' @details By default \code{flank_left} and \code{flank_right} will
-#' ignore strandedness of any ranges, while flank upstream and
-#' @seealso \code{\link[IRanges]{flank}}
+#' @details The function
+#' \code{flank_left} will create the flanking region to the left of starting
+#' coordinates in \code{x}, while \code{flank_right} will create the flanking
+#' region to the right of the starting coordinates in \code{x}. The function
+#' \code{flank_upstream} will \code{flank_left} if the strand of rows in \code{x} is
+#' not negative and will \code{flank_right} if the strand of rows in \code{x} is
+#' negative. The function \code{flank_downstream} will \code{flank_right} if the strand of rows in \code{x} is
+#' not negative and will \code{flank_leftt} if the strand of rows in \code{x} is
+#' negative.
+#'
+#' By default \code{flank_left} and \code{flank_right} will
+#' ignore strandedness of any ranges, while \code{flank_upstream} and
+#' \code{flank_downstream} will take into account the strand of \code{x}.
+#'
+#' @return A Ranges object of same length as \code{x}.
+#'
+#' @seealso \code{\link[IRanges]{flank}} \link[GenomicRanges]{`intra-range-methods`}
 #' @importFrom IRanges flank
 #' @examples
 #' gr <- as_granges(data.frame(start = 10:15,
