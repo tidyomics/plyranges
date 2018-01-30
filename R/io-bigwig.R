c7515e9519764a7409054ca8c1b997477e9a204c
Stuart Lee <lee.s@wehi.edu.au>
2018-01-30 14:34:02 +1100

    tests for bigwig files

--- R/io-bigwig.R
+++ R/io-bigwig.R
@@ -1,45 +1,50 @@
 #' Read a BigWig file
-#' @param file A path to a file or a connection.
+#' @param file A path to a file or URL.
 #' @param genome_info An optional character string or a Ranges object
-#' that contains information about the genome build. For example the USSC identifier
+#' that contains information about the genome build. For example the identifier
 #'"hg19" will add build information to the returned GRanges.
 #' @param overlap_ranges An optional Ranges object. Only the intervals in the file
 #' that overlap the Ranges will be loaded.
+#'
+#'
 #' @importFrom rtracklayer import.bw BigWigSelection BigWigFile
-#' @seealso \code{\link[rtracklayer]{BigWigFile}}
+#' @seealso \link[rtracklayer]{BigWigFile}
 #' @export
-#' @rdname bigwig-files-read
+#' @rdname io-bigwig-read
 read_bigwig <- function(file, genome_info = NULL, overlap_ranges = NULL) {
 
-  file <- BigWigFile(file)
+  stopifnot(is(overlap_ranges, "GRanges_OR_NULL"))
 
-  if (!is.null(overlap_ranges) & is(overlap_ranges, "GRanges")) {
-    selection <- BigWigSelection(overlap_ranges)
-  } else {
-    selection <- BigWigSelection(file)
-  }
-
-  if (is(genome_info, "GRanges")) {
-    seq_info <- seqinfo(genome_info)
-  } else if (!is.null(genome_info)){
-    seq_info <- Seqinfo(genome = genome_info)
+  if (!is.null(genome_info)) {
+    if (is(genome_info, "GRanges")) {
+      seq_info <- seqinfo(genome_info)
+    } else if (is.character(genome_info)) {
+      seq_info <- GenomeInfoDb::Seqinfo(genome = genome_info)
+    }
   } else {
     seq_info <- NULL
   }
 
-  rng <- import.bw(file, selection = selection)
-  if (!is.null(seq_info)) {
-    seqinfo(rng) <- seq_info
-    return(rng)
+
+  if (!is.null(overlap_ranges)) {
+    selection <- BigWigSelection(overlap_ranges)
+    rng <- import.bw(file, selection = selection, as = "GRanges")
+  } else {
+    rng <- import.bw(file, format = "bw", as = "GRanges")
   }
 
+  if (!is.null(seq_info)) {
+    seqinfo(rng) <- seq_info
+  }
   rng
 }
 
 #' Write a BED file
 #'
 #' @param x A GRanges object
-#' @param path Path or connection to write to
+#' @param file File name, URL or connection specifying a file to write x to.
+#'             Compressed files with extensions such as '.gz' are handled
+#'             automatically.
 #'
 #' @description This is a lightweight wrapper to the export family
 #' of functions defined in \pkg{rtracklayer}.
@@ -47,7 +52,7 @@ read_bigwig <- function(file, genome_info = NULL, overlap_ranges = NULL) {
 #' @importFrom rtracklayer export.bw
 #' @seealso \code{\link[rtracklayer]{BigWigFile}}
 #' @export
-#' @rdname bigwig-files-write
-write_bigwig <- function(x, path) {
-  export.bw(x, path)
+#' @rdname io-bigwig-write
+write_bigwig <- function(x, file) {
+  export.bw(x, file)
 }
