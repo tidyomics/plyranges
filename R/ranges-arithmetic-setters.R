c82ef43c209453c04bbab5968c32945f40dcb1df
Stuart Lee <lee.s@wehi.edu.au>
2018-04-29 22:01:37 +1000

    refactoring width to be part of setters; update tests

--- R/ranges-arithmetic-setters.R
+++ R/ranges-arithmetic-setters.R
@@ -1,5 +1,47 @@
 #' Internal methods for `mutate`
 #' functional setters for Ranges objects
+#' @importFrom IRanges resize
+#' @export
+set_width <- function(x, width) UseMethod("set_width")
+
+set_width.Ranges <- function(x, width = 0L) {
+  width(x) <- width
+  x
+}
+
+set_width.AnchoredIntegerRanges <- function(x, width = 0L) {
+  anchor <- anchor(x)
+  rng <- x@delegate
+  switch(anchor,
+         start = resize(rng, width, fix = "start"),
+         end = resize(rng, width, fix = "end"),
+         center = resize(rng, width, fix = "center")
+  )
+}
+
+
+set_width.AnchoredGenomicRanges <- function(x, width = 0L) {
+  anchor <- anchor(x)
+  rng <- x@delegate
+  switch(anchor,
+         start = resize(rng, width, fix = "start", ignore.strand = TRUE),
+         end = resize(rng, width, fix = "end", ignore.strand = TRUE),
+         center = resize(rng, width, fix = "center", ignore.strand = TRUE),
+         "3p" = resize_by_strand(rng, width, "3p"),
+         "5p" = resize_by_strand(rng, width, "5p")
+  )
+}
+
+# anchor by strand version of resize
+resize_by_strand <- function(x, width, anchor) {
+  # anchor_3p == fix by start for negative strand
+  if (anchor == "3p") {
+    return(resize(x, width, fix = "end", ignore.strand = FALSE))
+  } else {
+    return(resize(x, width, fix = "start", ignore.strand = FALSE))
+  }
+}
+
 #' @export
 set_start <- function(x, start = 0L) { UseMethod("set_start") }
 
--- tests/testthat/test-set-width.R
+++ tests/testthat/test-set-width.R
@@ -1,16 +1,13 @@
 context("resizing ranges")
 
-
-
-
 test_that("resizing with anchoring", {
   ir1 <- IRanges(c(2,5,1), c(3,7,3))
 
-  expect_identical(set_width(ir1, 10),
+  expect_identical(mutate(ir1, width = 10),
                  IRanges(c(2, 5, 1), width=10))
-  expect_identical(set_width(anchor_end(ir1), width = 10),
+  expect_identical(mutate(anchor_end(ir1), width = 10),
                  IRanges(c(-6, -2, -6), width=10))
-  expect_identical(set_width(anchor_center(ir1), 10),
+  expect_identical(mutate(anchor_center(ir1), width = 10),
                  IRanges(c(-2, 1, -3), width=10))
 
 
@@ -20,18 +17,18 @@ test_that("resizing with anchoring", {
                 strand = Rle(strand(c("-", "+", "*", "+", "-")),
                              c(1, 2, 2, 3, 2)))
   # ignores strand by defualt
-  gr_w <- set_width(gr, 10)
+  gr_w <- mutate(gr, width = 10)
   expect_identical(rep(10L, length(gr)), width(gr_w))
   expect_identical(c(1L:10L), start(gr_w))
-  ir_c <- ranges(set_width(anchor_center(gr), 10))
+  ir_c <- ranges(mutate(anchor_center(gr), width = 10))
   expect_identical(ir_c, IRanges(rep(1:5, each=2), width = 10))
   # anchoring at 5p will fix start on + and fix end on negative
-  ir_5p <- ranges(set_width(anchor_5p(gr), 10))
+  ir_5p <- ranges(mutate(anchor_5p(gr), width = 10))
   expect_identical(ir_5p,
                  IRanges(c(1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 1L, 1L),
                          width = 10L))
   # anchoring at 3p will fix start on - and fix end on +
-  ir_3p <- ranges(set_width(anchor_3p(gr), 10))
+  ir_3p <- ranges(mutate(anchor_3p(gr), width = 10))
   expect_identical(ir_3p,
                    IRanges(c(rep(1L, 8), 9, 10), width = 10L))
 
