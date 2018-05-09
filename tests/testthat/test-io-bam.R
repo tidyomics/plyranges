5396b2b3256dfb2f6abfff72b631a23d50c6644b
Stuart Lee <lee.s@wehi.edu.au>
2018-05-09 14:02:13 +1000

    fix tests

--- tests/testthat/test-io-bam.R
+++ tests/testthat/test-io-bam.R
@@ -4,11 +4,11 @@ test_that("overlaps works as expected", {
   fl <- system.file("extdata", "ex1.bam", package="Rsamtools")
   olap <- GRanges(seqnames = "seq1", ranges = IRanges(1, width = 100))
   result <- read_bam(fl) %>% filter_by_overlaps(olap)
-  expect_s4_class(result, "GRangesDeferred")
+  expect_s4_class(result, "DeferredGenomicRanges")
   # now has cache filled
-  expect_true(has_cache_contents(result))
+  expect_true(is_empty_delegate(result))
   expect_identical(c(seq1=1575L, seq2=1584L),
-                   seqlengths(get_cache(result)))
+                   seqlengths(load_delegate(result)))
 
 })
 
@@ -16,8 +16,8 @@ test_that("no index", {
   fl <- system.file("unitTests", "cases", "ex1_noindex.bam",
                     package="Rsamtools")
   result <- read_bam(fl, index = NULL)
-  expect_s4_class(result, "GRangesDeferred")
-  expect_false(has_cache_contents(result))
+  expect_s4_class(result, "DeferredGenomicRanges")
+  expect_true(is_empty_delegate(result))
   exp <- DataFrame(n = 3271L)
   expect_identical(exp,  result %>% summarise(n = n()))
 })
