c2e49c68850d2a3d829e7e7c4dce39f51907ae68
Stuart Lee <lee.s@wehi.edu.au>
2019-11-13 15:58:51 +1100

    update groups

--- R/ranges-overlap-groups.R
+++ R/ranges-overlap-groups.R
@@ -9,7 +9,7 @@ group_by_overlaps.IntegerRanges <- function(x, y, maxgap = -1L, minoverlap = 0L)
   hits <- make_hits(x, y, findOverlaps, maxgap = maxgap, minoverlap = minoverlap)
   left <- expand_by_hits(x, y, c(".query", ".subject"), hits)
   mcols(left)$query <- queryHits(hits)
-  new_grouped_ir(left, !!!rlang::syms("query"))
+  group_by(left, !!!rlang::syms("query"))
 }
 
 #' @rdname ranges-overlaps
@@ -21,7 +21,7 @@ group_by_overlaps.GenomicRanges <- function(x, y, maxgap = -1L, minoverlap = 0L)
                     ignore.strand = TRUE)
   left <- expand_by_hits(x, y, c(".query", ".subject"), hits)
   mcols(left)$query <- queryHits(hits)
-  new_grouped_gr(left, !!!rlang::syms("query"))
+  group_by(left, !!!rlang::syms("query"))
 }
 
 # TODO add in more variants here?
