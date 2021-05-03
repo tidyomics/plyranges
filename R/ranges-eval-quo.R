4519b8bfbd7d366c6bf966235348308d96e9ab75
sa-lee <lee.s@wehi.edu.au>
2021-05-03 13:23:20 +1000

    remove dead code

--- R/ranges-eval-quo.R
+++ R/ranges-eval-quo.R
@@ -1,13 +1,13 @@
-# this enables the use of `::` so we can dispatch on the 
+# this enables the use of `::` so we can dispatch on the
 # right generics
 
 #' @importFrom methods getGeneric getGenerics
 bioc_generics <- function(pkgs = c("BiocGenerics", "IRanges", "S4Vectors")) {
   pkgs <- lapply(pkgs, asNamespace)
   generics <-  getGenerics(pkgs)
-  fn <- mapply(getGeneric, 
-               f = generics@.Data, 
-               package = generics@package, 
+  fn <- mapply(getGeneric,
+               f = generics@.Data,
+               package = generics@package,
                SIMPLIFY = FALSE)
   Filter(function(x) {
     fun = try(x@default, silent = TRUE)
@@ -30,7 +30,7 @@ scope_plyranges <- function(env, generics) {
   }
   nms <- setdiff(nms, rlang::env_names(rlang::global_env()))
   generics <- generics[nms]
-  
+
   child <- rlang::child_env(env, !!!generics)
   child
 }
@@ -50,30 +50,9 @@ quos_set_env <- function(quos, generics = bioc_generics()) {
 set_dots_unnamed <- function(...) {
   dots <- rlang::enquos(...)
   quos_set_env(dots)
-} 
+}
 
 set_dots_named <- function(...) {
   dots <- rlang::enquos(..., .named = TRUE)
   quos_set_env(dots)
-}
-
-
-# A half baked algortihm for dynamicall parsing a call from a quosure
-# and mapping it over list objects
-quo_set_generic <- function(quo, generics = bioc_generics()) {
-  if (!rlang::quo_is_call(quo)) {
-    return(quo)
-  } else {
-      fn_name <- rlang::call_name(quo)
-      args_list <- rlang::call_args(quo)
-      quo_env <- rlang::quo_get_env(quo)
-      # idea here is to check for whether the function being called in the
-      # quosure has has a method (with appropriate signature) 
-      # in the quosoure environment
-      # if it does we just return the quo
-      # if it doesn't we add the generics from the bioc to quosoure environment
-      # and repeat the process 
-      # if there is still no method and if the signature is a list we modify
-      # the call to pass to a Map 
-  }
 }
